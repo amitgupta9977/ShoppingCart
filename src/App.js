bc46a4aaba675c6cc7b139834b05dbdaf647739d
glowtouchamit <144773019+glowtouchamit@users.noreply.github.com>
2024-06-21 19:46:02 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import Cart from "./components/Cart";
 import { Provider } from "react-redux";
 import store from "./store/store";
-//commented the code
+//commented the
 
 const App = () => {
   return (
